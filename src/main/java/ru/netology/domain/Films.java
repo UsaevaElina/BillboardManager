53ebcde098407393441e2cf31c26d7cb25bf6996
ElinaUsaeva <Usaeva1@yandex.ru>
2023-04-10 21:46:05 +0300

    Billboard manager and tests

---
+++ src/main/java/ru/netology/domain/Films.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+public class Films {
+    private String filmNames;
+    public Films(String filmNames) {
+        this.filmNames = filmNames;
+    }
+}
---
+++ src/test/java/TestFilms.java
@@ -0,0 +1,96 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.FilmManager;
+import ru.netology.domain.Films;
+
+public class TestFilms {
+
+    @Test
+    public void ShouldTestFilmsAdd(){
+        FilmManager manager = new FilmManager(5);
+        Films first = new Films("Джентельмены");
+        Films second = new Films("Отель Гранд Будапешт");
+        Films third = new Films("Шрек");
+        Films fourth = new Films("Лед");
+        Films fifth = new Films("Вперед");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+
+        Films[] actual = manager.findAll();
+        Films[] expected = new Films[]{first, second, third, fourth, fifth};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+   @Test
+    public void ShouldTestFilmsEmpty(){
+       FilmManager manager = new FilmManager(5);
+       Films[] actual = manager.findAll();
+       Films[] expected = new Films[]{};
+
+       Assertions.assertArrayEquals(expected, actual);
+   }
+   @Test
+    public void ShouldFindLastMoreMax(){
+       FilmManager manager = new FilmManager();
+       Films first = new Films("Джентельмены");
+       Films second = new Films("Отель Гранд Будапешт");
+       Films third = new Films("Шрек");
+       Films fourth = new Films("Лед");
+       Films fifth = new Films("Вперед");
+       Films sixth = new Films("Шрек Навсегда");
+
+       manager.add(first);
+       manager.add(second);
+       manager.add(third);
+       manager.add(fourth);
+       manager.add(fifth);
+       manager.add(sixth);
+
+       Films[] actual = manager.findLast();
+       Films[] expected = new Films[]{sixth,fifth,fourth,third,second};
+       Assertions.assertArrayEquals(expected, actual);
+   }
+
+   @Test
+   public void ShouldFindLastParam(){
+        FilmManager manager = new FilmManager(6);
+        Films first = new Films("Джентельмены");
+        Films second = new Films("Отель Гранд Будапешт");
+        Films third = new Films("Шрек");
+        Films fourth = new Films("Лед");
+        Films fifth = new Films("Вперед");
+        Films sixth = new Films("Шрек Навсегда");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+        manager.add(sixth);
+
+        Films[] actual = manager.findLast();
+        Films[] expected = new Films[]{sixth,fifth,fourth,third,second,first};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void ShouldFindLastParamFilmsLeight(){
+        FilmManager manager = new FilmManager();
+        Films first = new Films("Джентельмены");
+        Films second = new Films("Отель Гранд Будапешт");
+        Films third = new Films("Шрек");
+        Films fourth = new Films("Лед");
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+
+        Films[] actual = manager.findLast();
+        Films[] expected = new Films[]{fourth, third, second, first};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
